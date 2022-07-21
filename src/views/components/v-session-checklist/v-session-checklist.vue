07160cbaba49501423b82f89bf8e487eb3cc8ef1
Angel Manchev <angel@manchev.pro>
2022-07-21 18:31:10 +0300

    Update packages to latest versions

--- src/views/components/v-session-checklist/v-session-checklist.vue
+++ src/views/components/v-session-checklist/v-session-checklist.vue
@@ -7,22 +7,14 @@
             <q-item-section>{{ item.title }}</q-item-section>
             <q-item-section side>
                 <q-spinner-pie size="sm" v-if="item.state.value === 'loading'" />
-                <q-icon
-                    name="check_circle_outline"
-                    color="green"
-                    v-if="item.state.value === 'done'"
-                />
+                <q-icon name="check_circle_outline" color="green" v-if="item.state.value === 'done'" />
             </q-item-section>
         </q-item>
-        <q-inner-loading
-            :showing="showLoading"
-            label-class="text-teal"
-            label-style="font-size: 1.1em"
-        />
+        <q-inner-loading :showing="showLoading" label-class="text-teal" label-style="font-size: 1.1em" />
     </q-list>
 </template>
 <script setup lang="ts">
-import { defineProps, withDefaults, Ref } from 'vue'
+import { withDefaults, Ref } from 'vue'
 import { Item } from 'src/composables/use-session-list'
 
 interface Props {
